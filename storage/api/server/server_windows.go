asd1355215911/docker

api\server: Factored out UnixHttp on Windows, supported on Linux only.

api/server/server_windows.go
@@ -1,0 +1,19 @@
+// +build windows
+
+package server
+
+import (
+	"fmt"
+	"github.com/docker/docker/engine"
+)
+
+// NewServer sets up the required Server and does protocol specific checking.
+func NewServer(proto, addr string, job *engine.Job) (Server, error) {
+	// Basic error and sanity checking
+	switch proto {
+	case "tcp":
+		return setupTcpHttp(addr, job)
+	default:
+		return nil, fmt.Errorf("Invalid protocol format. Windows only supports tcp.")
+	}
+}

zvenigorodsky/container-libs

Signed-off-by: John Howard <John.Howard@microsoft.com>

storage/api/server/server_windows.go
@@ -1,0 +1,20 @@
+// +build windows
+
+package server
+
+import (
+	"fmt"
+
+	"github.com/docker/docker/engine"
+)
+
+// NewServer sets up the required Server and does protocol specific checking.
+func NewServer(proto, addr string, job *engine.Job) (Server, error) {
+	// Basic error and sanity checking
+	switch proto {
+	case "tcp":
+		return setupTcpHttp(addr, job)
+	default:
+		return nil, errors.New("Invalid protocol format. Windows only supports tcp.")
+	}
+}