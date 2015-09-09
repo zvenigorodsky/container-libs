zvenigorodsky/container-libs

commit 9d4066d1d253d90e7bf8659ae4375bfd9a1e8f43

Lint package pkg/devicemapper
Signed-off-by: Vincent Demeester <vincent@sbr.pm>

storage/pkg/devicemapper/devmapper_wrapper_deferred_remove.go
@@ -8,6 +8,7 @@ package devicemapper
 */
 import "C"
 
+// LibraryDeferredRemovalsupport is supported when statically linked.
 const LibraryDeferredRemovalSupport = true
 
 func dmTaskDeferredRemoveFct(task *CDmTask) int {

storage/daemon/graphdriver/devmapper/deviceset.go
@@ -266,7 +266,7 @@ func (devices *DeviceSet) allocateTransactionID() uint64 {
 }
 
 func (devices *DeviceSet) updatePoolTransactionID() error {
-	if err := devicemapper.SetTransactionId(devices.getPoolDevName(), devices.TransactionID, devices.OpenTransactionID); err != nil {
+	if err := devicemapper.SetTransactionID(devices.getPoolDevName(), devices.TransactionID, devices.OpenTransactionID); err != nil {
 		return fmt.Errorf("Error setting devmapper transaction ID: %s", err)
 	}
 	devices.TransactionID = devices.OpenTransactionID
@@ -596,7 +596,7 @@ func (devices *DeviceSet) createRegisterDevice(hash string) (*devInfo, error) {
 
 	for {
 		if err := devicemapper.CreateDevice(devices.getPoolDevName(), deviceID); err != nil {
-			if devicemapper.DeviceIdExists(err) {
+			if devicemapper.DeviceIDExists(err) {
 				// Device ID already exists. This should not
 				// happen. Now we have a mechianism to find
 				// a free device ID. So something is not right.
@@ -648,7 +648,7 @@ func (devices *DeviceSet) createRegisterSnapDevice(hash string, baseInfo *devInf
 
 	for {
 		if err := devicemapper.CreateSnapDevice(devices.getPoolDevName(), deviceID, baseInfo.Name(), baseInfo.DeviceID); err != nil {
-			if devicemapper.DeviceIdExists(err) {
+			if devicemapper.DeviceIDExists(err) {
 				// Device ID already exists. This should not
 				// happen. Now we have a mechianism to find
 				// a free device ID. So something is not right.

storage/pkg/devicemapper/attach_loopback.go
@@ -82,7 +82,7 @@ func openNextAvailableLoopback(index int, sparseFile *os.File) (loopFile *os.Fil
 	return loopFile, nil
 }
 
-// attachLoopDevice attaches the given sparse file to the next
+// AttachLoopDevice attaches the given sparse file to the next
 // available loopback device. It returns an opened *os.File.
 func AttachLoopDevice(sparseName string) (loop *os.File, err error) {
 
@@ -108,7 +108,7 @@ func AttachLoopDevice(sparseName string) (loop *os.File, err error) {
 	}
 
 	// Set the status of the loopback device
-	loopInfo := &LoopInfo64{
+	loopInfo := &loopInfo64{
 		loFileName: stringToLoopName(loopFile.Name()),
 		loOffset:   0,
 		loFlags:    LoFlagsAutoClear,

storage/pkg/devicemapper/devmapper_wrapper.go
@@ -44,23 +44,23 @@ import (
 )
 
 type (
-	CDmTask C.struct_dm_task
+	cdmTask C.struct_dm_task
 
-	CLoopInfo64 C.struct_loop_info64
-	LoopInfo64  struct {
-		loDevice           uint64 /* ioctl r/o */
-		loInode            uint64 /* ioctl r/o */
-		loRdevice          uint64 /* ioctl r/o */
-		loOffset           uint64
-		loSizelimit        uint64 /* bytes, 0 == max available */
-		loNumber           uint32 /* ioctl r/o */
-		loEncrypt_type     uint32
-		loEncrypt_key_size uint32 /* ioctl w/o */
-		loFlags            uint32 /* ioctl r/o */
-		loFileName         [LoNameSize]uint8
-		loCryptName        [LoNameSize]uint8
-		loEncryptKey       [LoKeySize]uint8 /* ioctl w/o */
-		loInit             [2]uint64
+	cLoopInfo64 C.struct_loop_info64
+	loopInfo64  struct {
+		loDevice         uint64 /* ioctl r/o */
+		loInode          uint64 /* ioctl r/o */
+		loRdevice        uint64 /* ioctl r/o */
+		loOffset         uint64
+		loSizelimit      uint64 /* bytes, 0 == max available */
+		loNumber         uint32 /* ioctl r/o */
+		loEncryptType    uint32
+		loEncryptKeySize uint32 /* ioctl w/o */
+		loFlags          uint32 /* ioctl r/o */
+		loFileName       [LoNameSize]uint8
+		loCryptName      [LoNameSize]uint8
+		loEncryptKey     [LoKeySize]uint8 /* ioctl w/o */
+		loInit           [2]uint64
 	}
 )
 
@@ -77,6 +77,7 @@ const (
 	LoopSetCapacity = C.LOOP_SET_CAPACITY
 )
 
+// LOOP consts. (?)
 const (
 	LoFlagsAutoClear = C.LO_FLAGS_AUTOCLEAR
 	LoFlagsReadOnly  = C.LO_FLAGS_READ_ONLY
@@ -85,6 +86,7 @@ const (
 	LoNameSize       = C.LO_NAME_SIZE
 )
 
+// DeviceMapper Udev consts. (?)
 const (
 	DmUdevDisableSubsystemRulesFlag = C.DM_UDEV_DISABLE_SUBSYSTEM_RULES_FLAG
 	DmUdevDisableDiskRulesFlag      = C.DM_UDEV_DISABLE_DISK_RULES_FLAG
@@ -92,6 +94,7 @@ const (
 	DmUdevDisableLibraryFallback    = C.DM_UDEV_DISABLE_LIBRARY_FALLBACK
 )
 
+// DeviceMapper mapped functions.
 var (
 	DmGetLibraryVersion       = dmGetLibraryVersionFct
 	DmGetNextTarget           = dmGetNextTargetFct
@@ -123,38 +126,38 @@ func free(p *C.char) {
 	C.free(unsafe.Pointer(p))
 }
 
-func dmTaskDestroyFct(task *CDmTask) {
+func dmTaskDestroyFct(task *cdmTask) {
 	C.dm_task_destroy((*C.struct_dm_task)(task))
 }
 
-func dmTaskCreateFct(taskType int) *CDmTask {
-	return (*CDmTask)(C.dm_task_create(C.int(taskType)))
+func dmTaskCreateFct(taskType int) *cdmTask {
+	return (*cdmTask)(C.dm_task_create(C.int(taskType)))
 }
 
-func dmTaskRunFct(task *CDmTask) int {
+func dmTaskRunFct(task *cdmTask) int {
 	ret, _ := C.dm_task_run((*C.struct_dm_task)(task))
 	return int(ret)
 }
 
-func dmTaskSetNameFct(task *CDmTask, name string) int {
+func dmTaskSetNameFct(task *cdmTask, name string) int {
 	Cname := C.CString(name)
 	defer free(Cname)
 
 	return int(C.dm_task_set_name((*C.struct_dm_task)(task), Cname))
 }
 
-func dmTaskSetMessageFct(task *CDmTask, message string) int {
+func dmTaskSetMessageFct(task *cdmTask, message string) int {
 	Cmessage := C.CString(message)
 	defer free(Cmessage)
 
 	return int(C.dm_task_set_message((*C.struct_dm_task)(task), Cmessage))
 }
 
-func dmTaskSetSectorFct(task *CDmTask, sector uint64) int {
+func dmTaskSetSectorFct(task *cdmTask, sector uint64) int {
 	return int(C.dm_task_set_sector((*C.struct_dm_task)(task), C.uint64_t(sector)))
 }
 
-func dmTaskSetCookieFct(task *CDmTask, cookie *uint, flags uint16) int {
+func dmTaskSetCookieFct(task *cdmTask, cookie *uint, flags uint16) int {
 	cCookie := C.uint32_t(*cookie)
 	defer func() {
 		*cookie = uint(cCookie)
@@ -162,15 +165,15 @@ func dmTaskSetCookieFct(task *CDmTask, cookie *uint, flags uint16) int {
 	return int(C.dm_task_set_cookie((*C.struct_dm_task)(task), &cCookie, C.uint16_t(flags)))
 }
 
-func dmTaskSetAddNodeFct(task *CDmTask, addNode AddNodeType) int {
+func dmTaskSetAddNodeFct(task *cdmTask, addNode AddNodeType) int {
 	return int(C.dm_task_set_add_node((*C.struct_dm_task)(task), C.dm_add_node_t(addNode)))
 }
 
-func dmTaskSetRoFct(task *CDmTask) int {
+func dmTaskSetRoFct(task *cdmTask) int {
 	return int(C.dm_task_set_ro((*C.struct_dm_task)(task)))
 }
 
-func dmTaskAddTargetFct(task *CDmTask,
+func dmTaskAddTargetFct(task *cdmTask,
 	start, size uint64, ttype, params string) int {
 
 	Cttype := C.CString(ttype)
@@ -182,7 +185,7 @@ func dmTaskAddTargetFct(task *CDmTask,
 	return int(C.dm_task_add_target((*C.struct_dm_task)(task), C.uint64_t(start), C.uint64_t(size), Cttype, Cparams))
 }
 
-func dmTaskGetDepsFct(task *CDmTask) *Deps {
+func dmTaskGetDepsFct(task *cdmTask) *Deps {
 	Cdeps := C.dm_task_get_deps((*C.struct_dm_task)(task))
 	if Cdeps == nil {
 		return nil
@@ -206,7 +209,7 @@ func dmTaskGetDepsFct(task *CDmTask) *Deps {
 	return deps
 }
 
-func dmTaskGetInfoFct(task *CDmTask, info *Info) int {
+func dmTaskGetInfoFct(task *cdmTask, info *Info) int {
 	Cinfo := C.struct_dm_info{}
 	defer func() {
 		info.Exists = int(Cinfo.exists)
@@ -223,7 +226,7 @@ func dmTaskGetInfoFct(task *CDmTask, info *Info) int {
 	return int(C.dm_task_get_info((*C.struct_dm_task)(task), &Cinfo))
 }
 
-func dmTaskGetDriverVersionFct(task *CDmTask) string {
+func dmTaskGetDriverVersionFct(task *cdmTask) string {
 	buffer := C.malloc(128)
 	defer C.free(buffer)
 	res := C.dm_task_get_driver_version((*C.struct_dm_task)(task), (*C.char)(buffer), 128)
@@ -233,7 +236,7 @@ func dmTaskGetDriverVersionFct(task *CDmTask) string {
 	return C.GoString((*C.char)(buffer))
 }
 
-func dmGetNextTargetFct(task *CDmTask, next unsafe.Pointer, start, length *uint64, target, params *string) unsafe.Pointer {
+func dmGetNextTargetFct(task *cdmTask, next unsafe.Pointer, start, length *uint64, target, params *string) unsafe.Pointer {
 	var (
 		Cstart, Clength      C.uint64_t
 		CtargetType, Cparams *C.char

storage/pkg/devicemapper/devmapper_wrapper_no_deferred_remove.go
@@ -2,13 +2,14 @@
 
 package devicemapper
 
+// LibraryDeferredRemovalsupport is not supported when statically linked.
 const LibraryDeferredRemovalSupport = false
 
-func dmTaskDeferredRemoveFct(task *CDmTask) int {
+func dmTaskDeferredRemoveFct(task *cdmTask) int {
 	// Error. Nobody should be calling it.
 	return -1
 }
 
-func dmTaskGetInfoWithDeferredFct(task *CDmTask, info *Info) int {
+func dmTaskGetInfoWithDeferredFct(task *cdmTask, info *Info) int {
 	return -1
 }

storage/pkg/devicemapper/ioctl.go
@@ -22,7 +22,7 @@ func ioctlLoopSetFd(loopFd, sparseFd uintptr) error {
 	return nil
 }
 
-func ioctlLoopSetStatus64(loopFd uintptr, loopInfo *LoopInfo64) error {
+func ioctlLoopSetStatus64(loopFd uintptr, loopInfo *loopInfo64) error {
 	if _, _, err := syscall.Syscall(syscall.SYS_IOCTL, loopFd, LoopSetStatus64, uintptr(unsafe.Pointer(loopInfo))); err != 0 {
 		return err
 	}
@@ -36,8 +36,8 @@ func ioctlLoopClrFd(loopFd uintptr) error {
 	return nil
 }
 
-func ioctlLoopGetStatus64(loopFd uintptr) (*LoopInfo64, error) {
-	loopInfo := &LoopInfo64{}
+func ioctlLoopGetStatus64(loopFd uintptr) (*loopInfo64, error) {
+	loopInfo := &loopInfo64{}
 
 	if _, _, err := syscall.Syscall(syscall.SYS_IOCTL, loopFd, LoopGetStatus64, uintptr(unsafe.Pointer(loopInfo))); err != 0 {
 		return nil, err

commit 1fc40bc3782ba2087002ac733e464baf37e4a95d

pkg: devicemapper: fix typo in function declaration
9d4066d1d253d90e7bf8659ae4375bfd9a1e8f43 introduced a typo in function
declaration, this patch fixes that.

Signed-off-by: Antonio Murdaca <runcom@linux.com>

storage/pkg/devicemapper/devmapper_wrapper_deferred_remove.go
@@ -11,11 +11,11 @@ import "C"
 // LibraryDeferredRemovalsupport is supported when statically linked.
 const LibraryDeferredRemovalSupport = true
 
-func dmTaskDeferredRemoveFct(task *CDmTask) int {
+func dmTaskDeferredRemoveFct(task *cdmTask) int {
 	return int(C.dm_task_deferred_remove((*C.struct_dm_task)(task)))
 }
 
-func dmTaskGetInfoWithDeferredFct(task *CDmTask, info *Info) int {
+func dmTaskGetInfoWithDeferredFct(task *cdmTask, info *Info) int {
 	Cinfo := C.struct_dm_info{}
 	defer func() {
 		info.Exists = int(Cinfo.exists)