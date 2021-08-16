zvenigorodsky/container-libs

commit 7ad9eaa880d9e047015e242725a7e48633791380

Follow symlinks if they exists
Fixes: https://github.com/containers/podman/issues/10187

Signed-off-by: Daniel J Walsh <dwalsh@redhat.com>

storage/types/utils.go
@@ -155,8 +155,17 @@ func getRootlessUID() int {
 }
 
 func expandEnvPath(path string, rootlessUID int) (string, error) {
+	var err error
 	path = strings.Replace(path, "$UID", strconv.Itoa(rootlessUID), -1)
-	return filepath.Clean(os.ExpandEnv(path)), nil
+	path = os.ExpandEnv(path)
+	newpath, err := filepath.EvalSymlinks(path)
+	if err != nil {
+		if !os.IsNotExist(err) {
+			return "", err
+		}
+		newpath = filepath.Clean(path)
+	}
+	return newpath, nil
 }
 
 func DefaultConfigFile(rootless bool) (string, error) {

storage/types/options_test.go
@@ -106,10 +106,8 @@ func TestGetRootlessStorageOpts2(t *testing.T) {
 	opts := StoreOptions{
 		RootlessStoragePath: "/$HOME/$UID/containers/storage",
 	}
-	storageOpts, err := getRootlessStorageOpts(2000, opts)
-
 	expectedPath := filepath.Join(os.Getenv("HOME"), "2000", "containers/storage")
-
+	storageOpts, err := getRootlessStorageOpts(2000, opts)
 	assert.NilError(t, err)
 	assert.Equal(t, storageOpts.GraphRoot, expectedPath)
 }

commit ca703ef414a75d8e1a58a1259d7d012920a2f226

types: on error fallback to filepath.Clean()
Now expandEnvPath always fallbacks to filepath.Clean() when
filepath.EvalSymlinks() fails.

commit 7ad9eaa880d9e047015e242725a7e48633791380 introduced the
regression.

This is useful in a user namespace when the permissions for the
storage path are not yet tweaked to allow access for all users.

Reported by: https://github.com/containers/buildah/pull/3450

Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

storage/types/utils.go
@@ -160,9 +160,6 @@ func expandEnvPath(path string, rootlessUID int) (string, error) {
 	path = os.ExpandEnv(path)
 	newpath, err := filepath.EvalSymlinks(path)
 	if err != nil {
-		if !os.IsNotExist(err) {
-			return "", err
-		}
 		newpath = filepath.Clean(path)
 	}
 	return newpath, nil