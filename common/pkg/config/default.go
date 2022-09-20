zvenigorodsky/container-libs

commit 50eb74a4b1156bd7fd711dfe2225592ae187473c

config: look user id instead of rootless
Change the check to look for the UID to decide whether to load the
default configuration files from the user directory instead of the
system path.

Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

common/pkg/config/default.go
@@ -180,7 +180,7 @@ func DefaultConfig() (*Config, error) {
 	}
 
 	defaultEngineConfig.SignaturePolicyPath = DefaultSignaturePolicyPath
-	if unshare.IsRootless() {
+	if unshare.GetRootlessUID() > 0 {
 		configHome, err := homedir.GetConfigHome()
 		if err != nil {
 			return nil, err
@@ -289,7 +289,7 @@ func defaultConfigFromMemory() (*EngineConfig, error) {
 			return nil, err
 		}
 	}
-	storeOpts, err := types.DefaultStoreOptions(unshare.IsRootless(), unshare.GetRootlessUID())
+	storeOpts, err := types.DefaultStoreOptions(unshare.GetRootlessUID() > 0, unshare.GetRootlessUID())
 	if err != nil {
 		return nil, err
 	}
@@ -427,7 +427,7 @@ func defaultConfigFromMemory() (*EngineConfig, error) {
 }
 
 func defaultTmpDir() (string, error) {
-	if !unshare.IsRootless() {
+	if unshare.GetRootlessUID() != 0 {
 		return getLibpodTmpDir(), nil
 	}
 

common/pkg/config/config_linux.go
@@ -23,7 +23,7 @@ func customConfigFile() (string, error) {
 	if path, found := os.LookupEnv("CONTAINERS_CONF"); found {
 		return path, nil
 	}
-	if unshare.IsRootless() {
+	if unshare.GetRootlessUID() > 0 {
 		path, err := rootlessConfigPath()
 		if err != nil {
 			return "", err
@@ -34,7 +34,7 @@ func customConfigFile() (string, error) {
 }
 
 func ifRootlessConfigPath() (string, error) {
-	if unshare.IsRootless() {
+	if unshare.GetRootlessUID() > 0 {
 		path, err := rootlessConfigPath()
 		if err != nil {
 			return "", err

commit 18803495e8712dd97bb28ee18301a91554e43dff

config: correct the check for root
originally the code had the condition "!unshare.IsRootless()", and
commit 50eb74a4b1156bd7fd711dfe2225592ae187473c introduced the issue
by replacing it with "unshare.GetRootlessUID() != 0".

Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

common/pkg/config/default.go
@@ -427,7 +427,7 @@ func defaultConfigFromMemory() (*EngineConfig, error) {
 }
 
 func defaultTmpDir() (string, error) {
-	if unshare.GetRootlessUID() != 0 {
+	if unshare.GetRootlessUID() == 0 {
 		return getLibpodTmpDir(), nil
 	}
 