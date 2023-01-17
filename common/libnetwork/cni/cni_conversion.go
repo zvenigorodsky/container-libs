zvenigorodsky/container-libs

commit 2fae6d9fd65ebd0f582a4d49eeb4846b7d6eb387

libnetwork: change lock path to tmpfs for root
The default /etc/containers/networks location might not be writeable,
while this breaks podman network create it does not need to break all
podman commands since the lock is created on libpod initialization.

ref https://github.com/containers/common/pull/1270

Signed-off-by: Paul Holzinger <pholzing@redhat.com>

common/libnetwork/cni/network.go
@@ -18,9 +18,11 @@ import (
 	"github.com/containers/common/libnetwork/types"
 	"github.com/containers/common/pkg/config"
 	"github.com/containers/storage/pkg/lockfile"
+	"github.com/containers/storage/pkg/unshare"
 	"github.com/sirupsen/logrus"
-	"golang.org/x/sys/unix"
 )
+
+const defaultRootLockPath = "/run/lock/podman-cni.lock"
 
 type cniNetwork struct {
 	// cniConfigDir is directory where the cni config files are stored.
@@ -81,19 +83,15 @@ type InitConfig struct {
 // NewCNINetworkInterface creates the ContainerNetwork interface for the CNI backend.
 // Note: The networks are not loaded from disk until a method is called.
 func NewCNINetworkInterface(conf *InitConfig) (types.ContainerNetwork, error) {
-	// TODO: consider using a shared memory lock
-	lock, err := lockfile.GetLockFile(filepath.Join(conf.CNIConfigDir, "cni.lock"))
+	// root needs to use a globally unique lock because there is only one host netns
+	lockPath := defaultRootLockPath
+	if unshare.IsRootless() {
+		lockPath = filepath.Join(conf.CNIConfigDir, "cni.lock")
+	}
+
+	lock, err := lockfile.GetLockFile(lockPath)
 	if err != nil {
-		// If we're on a read-only filesystem, there is no risk of
-		// contention. Fall back to a local lockfile.
-		if errors.Is(err, unix.EROFS) {
-			lock, err = lockfile.GetLockFile(filepath.Join(conf.RunDir, "cni.lock"))
-			if err != nil {
-				return nil, err
-			}
-		} else {
-			return nil, err
-		}
+		return nil, err
 	}
 
 	defaultNetworkName := conf.DefaultNetwork

common/libnetwork/netavark/network.go
@@ -94,8 +94,13 @@ type InitConfig struct {
 // NewNetworkInterface creates the ContainerNetwork interface for the netavark backend.
 // Note: The networks are not loaded from disk until a method is called.
 func NewNetworkInterface(conf *InitConfig) (types.ContainerNetwork, error) {
-	// TODO: consider using a shared memory lock
-	lock, err := lockfile.GetLockFile(filepath.Join(conf.NetworkConfigDir, "netavark.lock"))
+	// root needs to use a globally unique lock because there is only one host netns
+	lockPath := defaultRootLockPath
+	if unshare.IsRootless() {
+		lockPath = filepath.Join(conf.NetworkConfigDir, "netavark.lock")
+	}
+
+	lock, err := lockfile.GetLockFile(lockPath)
 	if err != nil {
 		return nil, err
 	}

common/libnetwork/netavark/const.go
@@ -4,3 +4,5 @@
 package netavark
 
 const defaultBridgeName = "podman"
+
+const defaultRootLockPath = "/run/lock/netavark.lock"

commit 4add344e52b01979389d92f6d7ad63ad8e79bdec

libnetwork/cni: mkdir network config dir
Commit 2fae6d9fd65e caused a regression, previously the lockfile
package create the config directory. Now this is no longer the case
so we have to create it explicitly.

Note that netavark already did this so no change is needed there.

This was reported by the RHEL QE team.

Signed-off-by: Paul Holzinger <pholzing@redhat.com>

common/libnetwork/cni/cni_conversion.go
@@ -327,6 +327,9 @@ func (n *cniNetwork) createCNIConfigListFromNetwork(network *types.Network, writ
 	}
 	cniPathName := ""
 	if writeToDisk {
+		if err := os.MkdirAll(n.cniConfigDir, 0o755); err != nil {
+			return nil, "", err
+		}
 		cniPathName = filepath.Join(n.cniConfigDir, network.Name+".conflist")
 		err = os.WriteFile(cniPathName, b, 0o644)
 		if err != nil {

common/libnetwork/cni/network.go
@@ -141,11 +141,15 @@ func (n *cniNetwork) DefaultNetworkName() string {
 
 func (n *cniNetwork) loadNetworks() error {
 	// check the mod time of the config dir
+	var modTime time.Time
 	f, err := os.Stat(n.cniConfigDir)
-	if err != nil {
+	// ignore error if the file does not exists
+	if err != nil && !errors.Is(err, os.ErrNotExist) {
 		return err
 	}
-	modTime := f.ModTime()
+	if err == nil {
+		modTime = f.ModTime()
+	}
 
 	// skip loading networks if they are already loaded and
 	// if the config dir was not modified since the last call

common/libnetwork/cni/config_test.go
@@ -128,6 +128,10 @@ var _ = Describe("Config", func() {
 		})
 
 		It("create two networks", func() {
+			// remove the dir here since we do not expect it to exists in the real context as well
+			// the backend will create it for us
+			os.RemoveAll(cniConfDir)
+
 			network := types.Network{}
 			network1, err := libpodNet.NetworkCreate(network, nil)
 			Expect(err).To(BeNil())