containers/image

Make bearerToken.Token private
No need to make it a public field now.

Should not change behavior.

Signed-off-by: Miloslav Trmač <mitr@redhat.com>

docker/docker_client.go
@@ -84,8 +84,9 @@ type extensionSignatureList struct {
 	Signatures []extensionSignature `json:"signatures"`
 }
 
+// bearerToken records a cached token we can use to authenticate.
 type bearerToken struct {
-	Token          string
+	token          string
 	AccessToken    string
 	ExpiresIn      int
 	IssuedAt       time.Time
@@ -159,13 +160,13 @@ func newBearerTokenFromJSONBlob(blob []byte) (*bearerToken, error) {
 	}
 
 	res := &bearerToken{
-		Token:       token.Token,
+		token:       token.Token,
 		AccessToken: token.AccessToken,
 		ExpiresIn:   token.ExpiresIn,
 		IssuedAt:    token.IssuedAt,
 	}
-	if res.Token == "" {
-		res.Token = res.AccessToken
+	if res.token == "" {
+		res.token = res.AccessToken
 	}
 	if res.ExpiresIn < minimumTokenLifetimeSeconds {
 		res.ExpiresIn = minimumTokenLifetimeSeconds
@@ -784,7 +785,7 @@ func (c *dockerClient) setupRequestAuth(req *http.Request, extraScope *authScope
 						c.tokenCache[cacheKey] = token
 					}()
 				}
-				registryToken = token.Token
+				registryToken = token.token
 			}
 			req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", registryToken))
 			return nil

zvenigorodsky/container-libs

image/docker/docker_client.go
@@ -86,8 +86,9 @@ type extensionSignatureList struct {
 	Signatures []extensionSignature `json:"signatures"`
 }
 
+// bearerToken records a cached token we can use to authenticate.
 type bearerToken struct {
-	Token          string
+	token          string
 	AccessToken    string
 	ExpiresIn      int
 	IssuedAt       time.Time
@@ -172,13 +173,13 @@ func newBearerTokenFromHTTPResponseBody(res *http.Response) (*bearerToken, error
 	}
 
 	bt := &bearerToken{
-		Token:       token.Token,
+		token:       token.Token,
 		AccessToken: token.AccessToken,
 		ExpiresIn:   token.ExpiresIn,
 		IssuedAt:    token.IssuedAt,
 	}
-	if bt.Token == "" {
-		bt.Token = bt.AccessToken
+	if bt.token == "" {
+		bt.token = bt.AccessToken
 	}
 	if bt.ExpiresIn < minimumTokenLifetimeSeconds {
 		bt.ExpiresIn = minimumTokenLifetimeSeconds
@@ -799,7 +800,7 @@ func (c *dockerClient) setupRequestAuth(req *http.Request, extraScope *authScope
 					token = *t
 					c.tokenCache.Store(cacheKey, token)
 				}
-				registryToken = token.Token
+				registryToken = token.token
 			}
 			req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", registryToken))
 			return nil