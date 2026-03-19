zvenigorodsky/container-libs

commit eb9cb52f1d9d895dbbf18bc41fd457dd21e4a349

storage.conf: add warning if both graphroot options are set
If both graphroot and rootless_storage_path we use graphroot so add a
warning in such case and mention rootless_storage_path is deprecated.

Signed-off-by: Paul Holzinger <pholzing@redhat.com>

storage/types/options.go
@@ -244,6 +244,9 @@ func LoadStoreOptions(opts LoadOptions) (StoreOptions, error) {
 	}
 
 	if config.Storage.GraphRoot != "" {
+		if config.Storage.RootlessStoragePath != "" {
+			logrus.Warn("Both rootless_storage_path and graphroot are set, using graphroot setting only, rootless_storage_path is deprecated and should be removed form the config")
+		}
 		graphRoot, err := expandEnvPath(config.Storage.GraphRoot, rootlessUID)
 		if err != nil {
 			return storeOptions, err

commit 5eb2c9c8d5f651d2b217de30dc8cf007cf3b462e

storage: fix typo in rootless_storage_path warning
Fixes: eb9cb52f1d ("storage.conf: add warning if both graphroot options are set")

Signed-off-by: Paul Holzinger <pholzing@redhat.com>

storage/types/options.go
@@ -244,7 +244,7 @@ func LoadStoreOptions(opts LoadOptions) (StoreOptions, error) {
 
 	if config.Storage.GraphRoot != "" {
 		if config.Storage.RootlessStoragePath != "" {
-			logrus.Warn("Both rootless_storage_path and graphroot are set, using graphroot setting only, rootless_storage_path is deprecated and should be removed form the config")
+			logrus.Warn("Both rootless_storage_path and graphroot are set, using graphroot setting only, rootless_storage_path is deprecated and should be removed from the config")
 		}
 		graphRoot, err := expandEnvPath(config.Storage.GraphRoot, rootlessUID)
 		if err != nil {